f15064e7df3bd4343e981eee71d1adafeb041381
rens-kj <rens@kjsoftware.nl>
2025-04-28 10:32:47 +0200

    added dual-wield container with fpm and nginx

---
+++ docker/nginx-fpm/start.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+if [ ! "$(ls -A /var/www/html/storage)" ]; then
+  echo "Initializing storage directory..."
+  cp -R /var/www/html/storage-init/. /var/www/html/storage
+  chown -R www-data:www-data /var/www/html/storage
+fi
+
+exec /usr/bin/supervisord -c /etc/supervisord.conf
